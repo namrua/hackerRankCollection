7b5c4840b60a7f31fb95cc9243c1230eba14a56e
namrua <moonloren9x@gmail.com>
2021-04-04 23:11:54 +0700

    first commit

---
+++ HackerRankCollections/HackerRankCollections/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace HackerRankCollections
+{
+    class Program
+    {
+        class SinglyLinkedListNode
+        {
+            public int data;
+            public SinglyLinkedListNode next;
+
+            public SinglyLinkedListNode(int nodeData)
+            {
+                this.data = nodeData;
+                this.next = null;
+            }
+        }
+
+        class SinglyLinkedList
+        {
+            public SinglyLinkedListNode head;
+            public SinglyLinkedListNode tail;
+
+            public SinglyLinkedList()
+            {
+                this.head = null;
+                this.tail = null;
+            }
+
+            public void InsertNode(int nodeData)
+            {
+                SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+                if (this.head == null)
+                {
+                    this.head = node;
+                }
+                else
+                {
+                    this.tail.next = node;
+                }
+
+                this.tail = node;
+            }
+        }
+
+        static void printLinkedList(SinglyLinkedListNode head)
+        {
+            while (head != null)
+            {
+                Console.WriteLine(head.data);
+                head = head.next;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            SinglyLinkedList llist = new SinglyLinkedList();
+
+            int llistCount = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < llistCount; i++)
+            {
+                int llistItem = Convert.ToInt32(Console.ReadLine());
+                llist.InsertNode(llistItem);
+            }
+
+            printLinkedList(llist.head);
+        }
+    }
+}
