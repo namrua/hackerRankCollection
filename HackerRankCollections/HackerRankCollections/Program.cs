64983f7cfec59bde62c3faca57b38da8f38407cf
namrua <moonloren9x@gmail.com>
2021-04-14 08:35:15 +0700

    905644

--- HackerRankCollections/HackerRankCollections/Program.cs
+++ HackerRankCollections/HackerRankCollections/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HackerRankCollections
 {
-    class Program
+    public class Program
     {
+        public static List<int> gradingStudents(List<int> grades)
+        {
+
+            for (int i = 0; i < grades.Count; i++)
+            {
+                if (grades[i] > 37 && grades[i] % 5 > 2)
+                {
+                    grades[i] += (5 - grades[i] % 5);
+                }
+            }
+            return grades;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+            List<int> grades = new List<int>();
+
+            for (int i = 0; i < gradesCount; i++)
+            {
+                int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+                grades.Add(gradesItem);
+            }
+
+            List<int> result = gradingStudents(grades);
+
         }
     }
 }
